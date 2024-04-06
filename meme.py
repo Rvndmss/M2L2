d7bf7fcd0f8fed8045aaccb0f34df65098f1de4a
Rvndmss <159981403+Rvndmss@users.noreply.github.com>
2024-04-06 13:14:38 +0800

    Add files via upload

---
+++ meme.py
@@ -0,0 +1,62 @@
+with open('text.txt', 'w', encoding='utf-8') as f:
+    print(f.write("DIMAS ANJAY MABAR PROPESIONAL SLEBEWWWW"))
+
+import discord
+from discord.ext import commands
+import os
+import random
+import requests
+
+intents = discord.Intents.default()
+
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# Dan inilah cara Kamu mengganti nama file dari variabel!
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def meme(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+                    picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+organik = ['sisa makanan','daun','bahan kimia','kulit makanan','sayuran']
+anorganik = ['plastik','stereofoam','kaca','botol plastik','ban','karet']
+
+#fungsi pilih sampah
+@bot.command()
+async def pilih_sampah(ctx):
+    await ctx.send('jenis sampah yang ingin kamu ketahui: ')
+    msg = await bot.wait_for("message")
+    if msg.content in organik:
+        await ctx.send('Buang ke sampah tempat sampah organik')
+        
+    elif msg.content in anorganik:
+        await ctx.send('Buang ke sampah tempat sampah anorganik')
+
+bot.run("MTIxODQxNjU4NDUwMzA3MDczMA.G5bQjs.XYLnFzJaihslGoV6BRhYc9wvUxfUkIWZwglDZE")
